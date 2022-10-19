adffc286a9c596ad8c4054d98670a97870aed8b4
sharsky <1341307644@qq.com>
2022-10-19 17:45:09 +0800

    🌟 feat: add aptosswap to allpools

--- examples/allpools/main.go
+++ examples/allpools/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/omnibtc/go-hippo-sdk/aggregator/aptosswap"
 	"math/big"
 
 	"github.com/coming-chat/go-aptos/aptosclient"
@@ -20,6 +21,7 @@ const TestNode = "https://fullnode.testnet.aptoslabs.com"
 const basiqPoolAddress = "0x4885b08864b81ca42b19c38fff2eb958b5e312b1ec366014d4afff2775c19aab"
 const auxPoolAddress = "0xe1d61154f57bbbb256bb6e3ea786102e7d5c9af481cb4d11b11e579f98218f27"
 const pontemAddress = "0x385068db10693e06512ed54b1e6e8f1fb9945bb7a78c28a45585939ce953f99e"
+const aptosPoolAddress = "0xa5d3ac4d429052674ed38adc62d010e52d7c24ca159194d17ddc196ddb7e480b"
 
 func main() {
 	client, err := aptosclient.Dial(context.Background(), TestNode)
@@ -40,6 +42,7 @@ func main() {
 			basiq.NewPoolProvider(client, basiqPoolAddress, coinListClient),
 			auxamm.NewPoolProvider(client, auxPoolAddress, coinListClient),
 			pontem.NewPoolProvider(client, pontemAddress, coinListClient),
+			aptosswap.NewPoolProvider(client, aptosPoolAddress, coinListClient),
 		},
 	)
 	coinX, ok := coinListClient.GetCoinInfoByFullName("0x498d8926f16eb9ca90cab1b3a26aa6f97a080b3fcbe6e83ae150b7243a00fb68::devnet_coins::DevnetBTC")
@@ -50,7 +53,7 @@ func main() {
 	if !ok {
 		panic("coiny not found")
 	}
-	inputAmount := big.NewInt(10000000000)
+	inputAmount := big.NewInt(100000000)
 	quotes, err := aggr.GetQuotes(inputAmount, coinX, coinY, 3, false, false)
 	panicErr(err)
 
